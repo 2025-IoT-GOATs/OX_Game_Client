c965a688f23b87cf64690a6dc94b1b9c4f9621f2
talCSHN <yujakinasakoon@gmail.com>
2025-05-11 20:29:00 +0900

    [feat] Login, Character Move

--- IoT_GOATs_First_Project_Client/OX_Game_Client/ViewModels/MainViewModel.cs
+++ IoT_GOATs_First_Project_Client/OX_Game_Client/ViewModels/MainViewModel.cs
@@ -23,43 +23,5 @@ namespace OX_Game_Client.ViewModels
         {
             
         }
-
-        public void Login(string name)
-        {
-            if (this.Name is null)
-            {
-                MessageBox.Show("이름을 입력하시오");
-                return;
-            }
-            Console.WriteLine($"UserName: {name}");
-            Thread clientThread = new Thread(clientLoginFunc);
-            clientThread.IsBackground = true;
-            clientThread.Start();
-            Console.WriteLine("죽음의 OX 게임 클라이언트 실행");
-            clientThread.Join();
-
-            //Console.ReadLine();
-
-        }
-        private void clientLoginFunc(object obj)
-        {
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            EndPoint serverEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
-
-            socket.Connect(serverEP);
-            while (true)
-            {
-                byte[] buf = Encoding.UTF8.GetBytes(this.Name);
-                socket.Send(buf);
-
-                byte[] recvBytes = new byte[1024];
-                int nRecv = socket.Receive(recvBytes);
-                string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
-
-                Console.WriteLine("Login Success log from Server : " + txt);
-            }
-            socket.Close();
-            Console.WriteLine("TCP Client Socket Closed");
-        }
     }
 }
--- IoT_GOATs_First_Project_Client/OX_Game_Client/Views/MainView.xaml.cs
+++ IoT_GOATs_First_Project_Client/OX_Game_Client/Views/MainView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MahApps.Metro.Controls;
 using OX_Game_Client.ViewModels;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace OX_Game_Client.Views
 {
@@ -22,11 +25,70 @@ namespace OX_Game_Client.Views
         {
             InitializeComponent();
         }
-        private void OnConfirmClick(object sender, RoutedEventArgs e)
+        private async void OnConfirmClick(object sender, RoutedEventArgs e)
         {
+            string userName = UserNameTextBox.Text;
+            if (string.IsNullOrEmpty(userName))
+            {
+                MessageBox.Show("이름을 입력하시오");
+                return;
+            }
+            await Login(userName);
             MainGrid.Children.Clear();
-            MainGrid.Children.Add(new Views.Empty());
+            MainGrid.Children.Add(new Views.InGameWindow(userName));
         }
+        public async Task Login(string name)
+        {
+            //if (string.IsNullOrEmpty(name))
+            //{
+            //    MessageBox.Show("이름을 입력하시오");
+            //    return;
+            //}
+            Console.WriteLine($"UserName: {name}");
+            //Thread clientThread = new Thread(clientLoginFunc);
+            //clientThread.IsBackground = true;
+            await Task.Run(() => clientLoginFunc(name));
+            Console.WriteLine("죽음의 OX 게임 클라이언트 실행");
+            //clientThread.Join();
 
+            //Console.ReadLine();
+
+        }
+        private void clientLoginFunc(string name)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            EndPoint serverEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
+
+            socket.Connect(serverEP);
+            try
+            {
+                byte[] buf = Encoding.UTF8.GetBytes(name);
+                socket.Send(buf);
+
+                byte[] recvBytes = new byte[1024];
+                int nRecv = socket.Receive(recvBytes);
+                string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
+
+                Console.WriteLine("Login Success log from Server : " + txt);
+
+                Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show("로그인 성공!");
+                });
+
+                socket.Close();
+                Console.WriteLine("TCP Client Socket Closed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+
+                Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show("에러 발생");
+                });
+            }
+            
+        }
     }
 }
